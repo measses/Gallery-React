da1174928432c4c5be536a67e901072935ba4183
Mert Araz <mertoreyiz1907@gmail.com>
2023-10-06 00:15:13 +0300

    navbar position finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,8 @@ const Navbar = () => {
   const handleLogoClick = () => {
     window.location.href = "/";
   };
-  
   return (
-    <nav className="bg-gradient-to-r from-blue-100 via-blue-200 to-purple-100 p-3 w-max mt- rounded-lg">
+    <nav className="bg-gradient-to-r from-blue-100 via-blue-200 to-purple-100 p-3 w-max mt- rounded-lg mt-2">
       <div className="container mx-auto flex items-center justify-between" onClick={handleLogoClick}>
         <div className="flex items-center cursor-pointer">
           <div className="bg-blue-500 text-white rounded-full w-12 h-12 flex items-center justify-center mr-2">
